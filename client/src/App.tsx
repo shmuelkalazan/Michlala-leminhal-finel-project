813c47226e3a8f5007808996c16eef20829850d2
Shmuel Kalazan <semikalazan@gmail.com>
2025-10-20 10:35:57 +0300

    feat/ add multiple luangueges

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useTranslation } from 'react-i18next';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng: 'en' | 'fr' | 'he' | 'es' |'ar') => {
+    i18n.changeLanguage(lng);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>{t('welcome')}</h1>
+      <p>{t('about')}</p>
+
+      <div style={{ marginTop: '20px' }}>
+        <button onClick={() => changeLanguage('en')}>English</button>
+        <button onClick={() => changeLanguage('he')}>עברית</button>
+        <button onClick={() => changeLanguage('fr')}>Français</button>
+        <button onClick={() => changeLanguage('es')}>Español</button>
+        <button onClick={() => changeLanguage('ar')}>العربية</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/i18n.ts
@@ -0,0 +1,32 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// Import translations directly
+import translationEN from './locales/en/translation.json';
+import translationFR from './locales/fr/translation.json';
+import translationHE from './locales/he/translation.json';
+import translationES from './locales/es/translation.json';
+import translationAR from './locales/ar/translation.json';
+
+
+const resources = {
+    en: { translation: translationEN },
+    fr: { translation: translationFR },
+    he: { translation: translationHE },
+    es: { translation: translationES },
+    ar: { translation: translationAR },
+} as const;
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources,
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false, 
+        },
+    });
+
+export default i18n;
