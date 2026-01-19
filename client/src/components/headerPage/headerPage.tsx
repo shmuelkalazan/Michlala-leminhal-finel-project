3408471ca401dab10233e61bbfd0f076f43e9b2d
Shmuel Kalazan <semikalazan@gmail.com>
2026-01-19 14:08:10 +0200

    feat/add logout

--- client/src/components/headerPage/headerPage.tsx
+++ client/src/components/headerPage/headerPage.tsx
@@ -1,12 +1,18 @@
 import styles from "./headerPage.module.scss";
 import SelectLanguage from "../selectLanguage/selectLanguage";
 import HeaderNavbar from "../headerNavbar/HeaderNavbar";
+import LogoutButton from "../logoutButton/LogoutButton";
 
 const HeaderPage = () => {
   return (
     <header className={styles.headerPageContainer}>
-      <SelectLanguage />
+      <div className={styles.leftSection}>
+        <SelectLanguage />
+      </div>
       <HeaderNavbar />
+      <div className={styles.rightSection}>
+        <LogoutButton />
+      </div>
     </header>
   );
 };
