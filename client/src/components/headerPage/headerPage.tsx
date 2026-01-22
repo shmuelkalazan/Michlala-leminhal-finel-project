2d17586bb42278d2f3abeb26fc61f0fdaae3357c
Shmuel Kalazan <semikalazan@gmail.com>
2026-01-22 10:31:53 +0200

    feat /add mobile responsive

--- client/src/components/headerPage/headerPage.tsx
+++ client/src/components/headerPage/headerPage.tsx
@@ -1,19 +1,44 @@
+import { useState } from "react";
 import styles from "./headerPage.module.scss";
 import SelectLanguage from "../selectLanguage/selectLanguage";
 import HeaderNavbar from "../headerNavbar/HeaderNavbar";
 import LogoutButton from "../logoutButton/LogoutButton";
 
 const HeaderPage = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
-    <header className={styles.headerPageContainer}>
-      <div className={styles.leftSection}>
-        <SelectLanguage />
-      </div>
-      <HeaderNavbar />
-      <div className={styles.rightSection}>
-        <LogoutButton />
-      </div>
-    </header>
+    <>
+      <header className={styles.headerPageContainer}>
+        <div className={styles.leftSection}>
+          <button 
+            className={styles.hamburgerButton}
+            onClick={toggleSidebar}
+            aria-label="Toggle menu"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <SelectLanguage />
+        </div>
+        <HeaderNavbar isSidebarOpen={isSidebarOpen} closeSidebar={closeSidebar} />
+        <div className={styles.rightSection}>
+          <LogoutButton />
+        </div>
+      </header>
+      {isSidebarOpen && (
+        <div className={styles.overlay} onClick={closeSidebar}></div>
+      )}
+    </>
   );
 };
 
